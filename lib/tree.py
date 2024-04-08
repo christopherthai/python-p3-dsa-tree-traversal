385868fb4102999510a15212130b3bbb984e34c7
Christopher Thai <christopherthai.nyli@gmail.com>
2024-04-07 22:20:02 -0400

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,15 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+
+        nodes_to_visit = [self.root]
+
+        while nodes_to_visit:
+            current_node = nodes_to_visit.pop()
+            if current_node["id"] == id:
+                return current_node
+
+            nodes_to_visit = nodes_to_visit + current_node["children"]
+        return None
